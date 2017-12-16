bbd1067450a4fb67feee9bffef09a3a6baed7de5
Saad Mirza <saadmirza009@gmail.com>
2017-12-16 14:05:40 +0500

    Codeigniter DataTables Working Code Example

---
+++ controllers/Welcome.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+	public function get_tweets()
+	{
+		$this->load->library('Datatables');
+	   $this->datatables->select('id,tweet')->from('tweets');
+	   echo $this->datatables->generate();
+	}
+}
